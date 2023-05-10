03bae64492647158917f400f0468e522a8453598
Kenneth Tero <ktero.dev@gmail.com>
2023-05-10 21:24:14 +0800

    add sendJoke function

--- bot.js
+++ bot.js
@@ -14,28 +14,32 @@ client.on('ready', () => {
 })
 
 client.on('messageCreate', async (message) => {
-
     if (message.content === '!joke') {
         console.log(`Sending joke...`)
-        try {
-            const response = await fetch(`https://v2.jokeapi.dev/joke/Dark,Pun?blacklistFlags=nsfw,religious,political,racist,sexist,explicit`)
-            const json = await response.json()
-            const joke = json.joke
-
-            await message.channel.sendTyping()
-
-            if(typeof joke !== 'undefined') {
-                message.channel.send(joke)
-            } else {
-                const setup = json.setup
-                const delivery = json.delivery
-                message.channel.send(setup)
-                message.channel.send(`...${delivery}`)
-            }
-        } catch (error) {
-            console.log(`Error: ${error}`)
-        }
+        sendJoke(message.channel)
     }
 })
 
 client.login(process.env.DISCORD_TOKEN)
+
+
+async function sendJoke(channel) {
+    try {
+        const response = await fetch(`https://v2.jokeapi.dev/joke/Dark,Pun?blacklistFlags=nsfw,religious,political,racist,sexist,explicit`)
+        const json = await response.json()
+        const joke = json.joke
+
+        await channel.sendTyping()
+
+        if(typeof joke !== 'undefined') {
+            channel.send(joke)
+        } else {
+            const setup = json.setup
+            const delivery = json.delivery
+            channel.send(setup)
+            channel.send(`...${delivery}`)
+        }
+    } catch (error) {
+        console.log(`Error: ${error}`)
+    }
+}
